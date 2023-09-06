bdb3ab8657456839c989bdc623dfefb01ff9d55c
santyru <santiago0988@hotmail.com>
2023-09-06 11:48:50 -0300

    implement fibonacci

--- guia-4/guia-4.hs
+++ guia-4/guia-4.hs
@@ -3,7 +3,15 @@
 -- CASO BASE
 -- LLAMADO RECURSIVO QUE REDUZCA AL CASO BASE
 
+---
+
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci x = fibonacci (x - 1) + fibonacci (x - 2)
+
 --
+
 {-
 problema esDivisible (a:N, b:N): Bool {
     requiere: {True}
@@ -17,7 +25,18 @@ esDivisible a b
     | a - b > 0 = (esDivisible (a - b) b)
     | otherwise = False 
 
---
+---
+
+{-
+problema sumaImpares (n:N): sumaImp:N {
+    requiere: {True}
+    asegura: {res = la suma de los primeros n numeros impares}
+}
+-}
+
+
+---
+
 todosDigitosIguales :: Integer -> Bool
 todosDigitosIguales x
     | head == 0 = True
