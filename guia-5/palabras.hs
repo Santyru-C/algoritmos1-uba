76de6948f031d317a3fc00dd7fc4f395c2c9920c
santyru <santiago0988@hotmail.com>
2023-09-11 09:11:44 -0300

    add sumatoria

--- guia-5/palabras.hs
+++ guia-5/palabras.hs
@@ -4,6 +4,13 @@ longitud :: [Integer] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
+-- indica la suma de todos los elementos de una lista
+
+sumatoria :: [Integer] -> Integer
+sumatoria [] = 0
+sumatoria (x:xs) = x + sumatoria xs
+
+-- indica si un elemento pertenece a una lista
 test :: [Integer] -> [Integer]
 test (x:xs) = xs
 
