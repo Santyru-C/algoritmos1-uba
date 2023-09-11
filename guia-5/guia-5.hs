eb8f97535a146082202a7202ae63563bffa4de5a
santyru <santiago0988@hotmail.com>
2023-09-11 10:46:23 -0300

    add comments for all functions from section 1

--- guia-5/guia-5.hs
+++ guia-5/guia-5.hs
@@ -1,20 +1,22 @@
 --- 1
--- a
+-- a -devuelve la cantidad de elementos de una lista de elementos tipo t
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (x:xs) = 1 + longitud xs
 
--- b
+-- b -devuelve el ultimo elementos de una lista de elementos tipo t
 ultimo :: [t] -> t
 ultimo (x:[]) = x
 ultimo (x:xs) = ultimo xs
 
--- c
+-- c -devuelve una sublista que contiene todos los elementos de la lista original exceptuando el ultimo
 principio :: [t] -> [t]
 principio (x:[]) = []
 principio (x:xs) = x:(principio xs)
 
--- d
+-- d -devuelve una lista con los mismos elementos que la lista de elementos t dada pero en orden inverso
 reverso :: [t] -> [t]
 reverso (x:[]) = [x]
 reverso (x:xs) = reverso(xs)++[x] -- using ++ as the function returns a list as a requirement
+
+--- 2
