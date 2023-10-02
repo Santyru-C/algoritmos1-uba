50b3643aa00bc03954ad4ad4afa15d87fe57c72f
santyru <santiago0988@hotmail.com>
2023-10-02 12:41:34 -0300

    implement imprimir_saludo

--- Guia_6/soluciones.py
+++ Guia_6/soluciones.py
@@ -1,21 +1,31 @@
 import math
+##1
 
+#a
 def imprimir_hola_mundo() -> str:
     print("¡Hola Mundo!")
 
-
+#b
 def imprimir_un_verso() -> str:
     print("Un elefante se balanceaba sobre la tela de una araña\nComo veia que resistia fueron a llamar a otro elefante")
 
+#c
 def raizDe2() -> float:
     res: float = round(2 ** (1/2), 2)
     return res
 
+#d
 def factorial_de_dos() -> int:
     return 2
 
+#e
 def perimetro() -> float:
     res: float = 2 * math.pi
     return res
+
+##2
+#a
+def imprimir_saludo(nombre: str) -> str:
+    print("Hola " + nombre)
 
 print(perimetro())
