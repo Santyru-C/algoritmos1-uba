3ed32f62dc64e1f3c41d27f6667e9359e3ee1949
santyru <santiago0988@hotmail.com>
2023-10-25 09:34:52 -0300

    implement existe_palabra

--- Guia_8/soluciones.py
+++ Guia_8/soluciones.py
@@ -1,6 +1,6 @@
 ### ARCHIVOS
 ##1
-#1
+#a
 def contar_lineas(nombre_archivo: str) -> int:
     archivo = open(f"{nombre_archivo}.txt", "r")
     
@@ -8,4 +8,21 @@ def contar_lineas(nombre_archivo: str) -> int:
     archivo.close()
     return len(line_list)
 
-print(contar_lineas("test"))
+#b
+def existe_palabra(palabra:str ,nombre_archivo: str) -> bool:
+    with open(f"{nombre_archivo}.txt", "r", encoding="utf-8") as archivo:
+        line_list: [str] = archivo.readlines()
+
+        for line in line_list:
+            if palabra in line:
+                return True
+            
+        return False
+    
+#c
+def cantidad_de_apariciones(nombre_archivo: str, palabra: str) -> int:
+    with open(nombre_archivo, "r", encoding="utf-8") as archivo:
+        line_list: [str] = archivo.readlines()
+        print(line_list)
+
+print(cantidad_de_apariciones("test.txt", "juan"))
