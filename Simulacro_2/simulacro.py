ed23bba6390a190314ff29a18a6106490505ea7a
santyru <santiago0988@hotmail.com>
2023-11-05 12:16:02 -0300

    finish implementation

--- Simulacro_2/simulacro.py
+++ Simulacro_2/simulacro.py
@@ -2,8 +2,8 @@ s1 = [-1, 4, 0, 4, 100, 0, 100, 0, -1, -1]
 e1 = 0
 
 def ultima_aparicion(s:[int], e: int) -> int:
-    # e pertenece a s
     # res es la posicion de la ultima aparicionde e en s
+    # e pertenece a s
     for i in range(len(s) - 1, -1, -1):
         if e == s[i]:
             return i
@@ -42,4 +42,43 @@ def elementos_exclusivos(s:[int], t:[int]) -> [int]:
 s2 = [1,1,1,2,3,4,4,5,6,9,9,8]
 t2 = [5,6,7,8,8,8]
 
-print(elementos_exclusivos(s2, t2))
+#print(elementos_exclusivos(s2, t2))
+
+def contar_traducciones_iguales(ing: dict, ale: dict) -> int:
+    # res = cantidad de palabras que están en ambos diccionarios y ademas tienen igual valor en ambos
+    d_mas_claves: dict = None
+    d_menos_claves: dict = None
+    contador: int = 0
+    if ing.keys() > ale.keys():
+        d_mas_claves = ing
+        d_menos_claves = ale
+    else:
+        d_mas_claves = ale
+        d_menos_claves = ing
+
+    for key in d_mas_claves:
+        if key in d_menos_claves.keys():
+            if d_mas_claves[key] == d_menos_claves[key]:
+                contador += 1
+    
+    return contador
+
+aleman = {"Mano": "Hand", "Pie": "Fuss", "Dedo": "Finger", "Cara": "Gesicht"}
+ingles = {"Pie": "Foot", "Dedo": "Finger", "Mano": "Hand"}
+
+#print(contar_traducciones_iguales(aleman, ingles))
+
+def convertir_a_diccionario(l: [int]) -> dict[int, int]:
+    # res tiene como claves los elementos de lista y res[n] = cantidad de veces que aparece n en la lista.
+    d: dict[int, int] = {}
+
+    for elem in l:
+        if not elem in d.keys():
+            d[elem] = 1
+        else:
+            d[elem] += 1
+
+    return d
+
+l = [-1, 0, 4, 100, -1, -1]
+print(convertir_a_diccionario(l))
